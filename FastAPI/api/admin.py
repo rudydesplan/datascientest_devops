8c27029ab8ec2ce1e6bf54b37e37657f8ac3254a
rudy desplan <desrudy@hotmail.com>
2023-07-14 16:38:01 +0200

    Update admin.py

--- FastAPI/api/admin.py
+++ FastAPI/api/admin.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, Depends, HTTPException
 from database import write_data_to_csv
-from models import QuestionIn
+from models import QuestionCreate
 from auth import get_current_username, ADMIN_USER
 
 router = APIRouter()
 
 @router.post("/")
-async def create_question(question: QuestionIn, current_user: str = Depends(get_current_username)):
+async def create_question(question: QuestionCreate, current_user: str = Depends(get_current_username)):
     if current_user != ADMIN_USER:
         raise HTTPException(status_code=403, detail="Operation not allowed")
     write_data_to_csv(question.dict())
