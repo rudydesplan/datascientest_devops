5b39aff1e16ccafcf705cb5fd51533b2ad834d64
rudy desplan <desrudy@hotmail.com>
2023-07-14 16:13:31 +0200

    Create utils.py

---
+++ FastAPI/api/utils.py
@@ -0,0 +1,11 @@
+from typing import List
+from config import config
+
+def validate_subjects(subjects: List[str]):
+    for subject in subjects:
+        if subject not in config.VALID_SUBJECTS:
+            raise ValueError(f"Invalid subject: {subject}")
+
+def validate_use_case(use_case: str):
+    if use_case not in config.VALID_USE_CASES:
+        raise ValueError(f"Invalid use case: {use_case}")
