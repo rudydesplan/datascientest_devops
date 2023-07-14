066890b58d8aa0d174d6fb9c69a75ee2229e0c4a
rudy desplan <desrudy@hotmail.com>
2023-07-14 16:48:30 +0200

    Update questions.py

--- FastAPI/api/questions.py
+++ FastAPI/api/questions.py
@@ -3,12 +3,12 @@ from fastapi import APIRouter, Depends, HTTPException
 from database import get_random_questions
 from models import Question
 from utils import validate_subjects, validate_use_case
-from auth import get_current_username
+from auth import get_current_user
 
 router = APIRouter()
 
 @router.get("/", response_model=List[Question])
-async def read_questions(subjects: List[str], use: str, number: int, current_user: str = Depends(get_current_username)):
+async def read_questions(subjects: List[str], use: str, number: int, current_user: str = Depends(get_current_user)):
     validate_subjects(subjects)
     validate_use_case(use)
     try:
