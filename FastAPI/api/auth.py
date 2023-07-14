17a53fd46e76b327be3ebdba3186b2438b2c19dc
rudy desplan <desrudy@hotmail.com>
2023-07-14 16:47:40 +0200

    Update auth.py

--- FastAPI/api/auth.py
+++ FastAPI/api/auth.py
@@ -5,11 +5,17 @@ from config import config
 security = HTTPBasic()
 
 def get_current_user(credentials: HTTPBasicCredentials = Depends(security)):
-    correct_username = config.AUTHORIZED_USERS.get(credentials.username)
-    if not correct_username or correct_username != credentials.password:
+    if credentials.username not in config.AUTHORIZED_USERS:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
+            detail="Incorrect username",
             headers={"WWW-Authenticate": "Basic"},
         )
-    return credentials.username
+    correct_password = config.AUTHORIZED_USERS.get(credentials.username)
+    if not correct_password or correct_password != credentials.password:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username, credentials.password
