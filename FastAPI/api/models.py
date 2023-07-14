5fb23e785c27667c24b5b4ff4c8a9e5c0dd2d7b0
rudy desplan <desrudy@hotmail.com>
2023-07-14 14:24:51 +0200

    Create models.py

---
+++ FastAPI/api/models.py
@@ -0,0 +1,37 @@
+from typing import Optional, List
+from pydantic import BaseModel
+
+class Question(BaseModel):
+    question: str
+    subject: str
+    correct: List[str]
+    use: str
+    answerA: str
+    answerB: str
+    answerC: str
+    answerD: Optional[str] = None
+    remark: Optional[str] = None
+
+class QuestionCreate(BaseModel):
+    question: str
+    subject: str
+    correct: List[str]
+    use: str
+    answerA: str
+    answerB: str
+    answerC: str
+    answerD: Optional[str] = None
+    remark: Optional[str] = None
+
+class TestType(BaseModel):
+    use: str
+
+class TestSubjects(BaseModel):
+    subjects: List[str]
+
+class NumberOfQuestions(BaseModel):
+    number: int
+
+class AuthenticationUser(BaseModel):
+    username: str
+    password: str
