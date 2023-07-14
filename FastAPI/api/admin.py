5c39514748722f1185f3a97c4c893897471454f3
rudy desplan <desrudy@hotmail.com>
2023-07-14 17:02:36 +0200

    Update admin.py

--- FastAPI/api/admin.py
+++ FastAPI/api/admin.py
@@ -6,7 +6,8 @@ from config import config
 
 router = APIRouter()
 
-@router.post("/", summary="Create a New Question")
+@router.post("/", summary="Create a New Question", 
+    description="Allows an admin user to create a new question. The details of the question must be provided in the request body.")
 async def create_question(question: QuestionCreate, current_user: str = Depends(get_current_user)):
     if current_user[0] != config.ADMIN_USERNAME or current_user[1] != config.ADMIN_PASSWORD:
         raise HTTPException(status_code=403, detail="Operation not allowed")
