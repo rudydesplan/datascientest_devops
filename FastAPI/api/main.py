ee5aa6ecac27138df2bd573de181f7ad43d51d35
rudy desplan <desrudy@hotmail.com>
2023-07-14 14:02:22 +0200

    Update main.py

--- FastAPI/api/main.py
+++ FastAPI/api/main.py
@@ -17,20 +17,26 @@ def ping():
 @app.get("/questions", response_model=List[Question])
 def get_questions(use: str, subject: List[str], count: int, credentials: HTTPBasicCredentials = Depends(security)):
     if not auth.verify_credentials(credentials):
-        raise HTTPException(status_code=401, detail="Invalid credentials")
+        raise HTTPException(status_code=401, detail="Invalid user credentials")
     if use not in config.VALID_USES:
         raise HTTPException(status_code=400, detail="Invalid use type")
     if not all(s in config.VALID_SUBJECTS for s in subject):
         raise HTTPException(status_code=400, detail="Invalid subject")
     if count not in config.VALID_COUNTS:
         raise HTTPException(status_code=400, detail="Invalid count")
-    return questions.get_questions(use, subject, count)
+    try:
+        return questions.get_questions(use, subject, count)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
-# Add a route for adding a new question
 @app.post("/admin/questions", response_model=Question)
 def create_question(question: QuestionBase, credentials: HTTPBasicCredentials = Depends(security)):
     if not auth.verify_credentials(credentials, admin=True):
-        raise HTTPException(status_code=401, detail="Invalid credentials")
-    return admin.create_question(question)
-
+        raise HTTPException(status_code=401, detail="Invalid admin credentials")
+    try:
+        return admin.create_question(question)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Error occurred while adding question")
+        
 # You will also need to define the Question and QuestionBase models in the models.py file.
+# this is still a basic setup and might need further adjustments based on the actual implementation of the auth, admin, database, and questions modules and your specific requirements.
